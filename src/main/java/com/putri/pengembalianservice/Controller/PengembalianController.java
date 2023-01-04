a7f8235b268414e7fdd241ef3a8e3a7b160b83b7
Ptrwa <113502703+Ptrwa@users.noreply.github.com>
2023-01-04 09:35:20 +0700

    Add files via upload

---
+++ src/main/java/com/putri/pengembalianservice/Controller/PengembalianController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.putri.pengembalianservice.Controller;
+
+import com.putri.pengembalianservice.Entity.Pengembalian;
+import com.putri.pengembalianservice.Service.PengembalianService;
+import com.putri.pengembalianservice.Vo.ResponseTemplateVo;
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author hp
+ */
+@RestController
+@RequestMapping("/pengembalian")
+public class PengembalianController {
+    @Autowired
+    private PengembalianService pengembalianService;
+    
+    @PostMapping("/")
+    public Pengembalian savePengembalian(@RequestBody Pengembalian pengembalian) throws ParseException{
+        return pengembalianService.savePengembalian(pengembalian);
+    }
+    
+    @GetMapping("{id}")
+    public ResponseTemplateVo getPengembalian(@PathVariable("id") Long pengembalianId){
+     return pengembalianService.getPengembalian(pengembalianId);
+    }
+}
---
+++ src/main/java/com/putri/pengembalianservice/PengembalianServiceApplication1.java
@@ -0,0 +1,26 @@
+package com.putri.pengembalianservice;
+
+import java.text.SimpleDateFormat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class PengembalianServiceApplication1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PengembalianServiceApplication1.class, args);
+	}
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    @Bean
+    public SimpleDateFormat simpleDateFormat() {
+        return new SimpleDateFormat("yyy-MM-dd");
+    }
+
+}
